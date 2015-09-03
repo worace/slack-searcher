fca9ae4211206ccebd17244482e8d42c4b85806e
worace <horace.d.williams@gmail.com>
2015-09-02 22:25:55 -0600

    some deps

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
+                 [hiccup "1.0.5"]
+                 [clojurewerkz/elastisch "2.1.0"]
                  [ring/ring-defaults "0.1.2"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler slack-searcher.handler/app}
